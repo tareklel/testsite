193245b414c9bfb07a80f826190ce704e7f63508
Tarek Lel <your.email@domain.com>
2019-12-21 18:06:54 +0400

    test site for practicing React

---
+++ react-client/src/Components/FooterComponent/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class FooterBar extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <p>This is an uninformative footer component</p>
+        </div>
+      </footer>
+  );
+  }
+}
+export default FooterBar;
---
+++ react-client/src/Components/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello this is my test site.</p>
+      </div>
+  );
+  }
+}
+export default HomePage;
